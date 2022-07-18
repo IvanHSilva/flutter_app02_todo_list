a529f55b247d9adaefeadaf5e57194970c4e55ea
IvanHSilva <ivanhenriquessilva@gmail.com>
2022-07-18 17:45:02 -0300

    Project created

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: ToDoList(),
+    );
+  }
+}
+
+class ToDoList extends StatelessWidget {
+  const ToDoList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.green,
+    );
+  }
+}
