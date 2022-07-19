de62827ec01733c7ddc67733ea04385b1e3cb034
IvanHSilva <ivanhenriquessilva@gmail.com>
2022-07-19 18:24:04 -0300

    Textfield added

--- lib/pages/todolist.dart
+++ lib/pages/todolist.dart
@@ -5,8 +5,30 @@ class ToDoList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.green,
+    return const Scaffold(
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: TextField(
+            decoration: InputDecoration(
+              labelText: 'E-Mail',
+              hintText: 'email@email.com',
+              border: OutlineInputBorder(),
+              //prefixText: 'R\$ ',
+              //suffixText: ' cm',
+              labelStyle: TextStyle(
+                fontSize: 20,
+              ),
+              errorText: null,
+            ),
+            keyboardType: TextInputType.emailAddress,
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
