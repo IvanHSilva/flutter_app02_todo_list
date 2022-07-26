245831e988234e1b5780112bfd9ede96884b2cc0
IvanHSilva <ivanhenriquessilva@gmail.com>
2022-07-26 17:03:30 -0300

    Footer text and button

--- lib/pages/todolist.dart
+++ lib/pages/todolist.dart
@@ -12,32 +12,55 @@ class ToDoList extends StatelessWidget {
         // Row tem largura infinita
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 16.0),
-          child: Row(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
             children: [
-              // Permte extender o widget com a máxima largura disponível
-              const Expanded(
-                child: TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Adione uma tarefa',
-                    hintText: 'Estudar programação',
+              Row(
+                children: [
+                  // Permite extender o widget com a máxima largura disponível
+                  const Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Adione uma tarefa',
+                        hintText: 'Estudar programação',
+                      ),
+                    ),
                   ),
-                ),
+                  const SizedBox(width: 8),
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      //primary: Colors.green,
+                      primary: const Color(0XFF58D8B5),
+                      fixedSize: const Size(50, 50),
+                      padding: const EdgeInsets.all(16),
+                    ),
+                    //child: const Text('+'),
+                    child: const Icon(
+                      Icons.add,
+                      size: 30,
+                    ),
+                  ),
+                ],
               ),
-              const SizedBox(width: 8),
-              ElevatedButton(
-                onPressed: () {},
-                style: ElevatedButton.styleFrom(
-                  //primary: Colors.green,
-                  primary: const Color(0XFF58D8B5),
-                  fixedSize: const Size(50, 50),
-                  padding: const EdgeInsets.all(16),
-                ),
-                //child: const Text('+'),
-                child: const Icon(
-                  Icons.add,
-                  size: 30,
-                ),
+              const SizedBox(height: 20),
+              Row(
+                children: [
+                  const Expanded(
+                    child: Text('0 tarefas pendentes'),
+                  ),
+                  const SizedBox(width: 8),
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      //primary: Colors.green,
+                      primary: Colors.blue,
+                      padding: const EdgeInsets.all(15),
+                    ),
+                    child: const Text('Limpar'),
+                  )
+                ],
               ),
             ],
           ),
