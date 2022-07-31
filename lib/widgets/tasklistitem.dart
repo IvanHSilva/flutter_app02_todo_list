7c2d614265ab8ac22eaa2c2a5d38560a24853373
IvanHSilva <ivanhenriquessilva@gmail.com>
2022-07-31 13:37:14 -0300

    Slidable widget added and tested

--- lib/widgets/tasklistitem.dart
+++ lib/widgets/tasklistitem.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:intl/intl.dart';
 
 import '../models/task.dart';
+// flutter pub add flutter_slidable
 
 class TaskListItem extends StatelessWidget {
   const TaskListItem({Key? key, required this.task}) : super(key: key);
@@ -10,30 +12,61 @@ class TaskListItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(16),
-        color: Colors.grey[200],
-      ),
-      margin: const EdgeInsets.symmetric(vertical: 3),
-      padding: const EdgeInsets.all(16.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
+    return Slidable(
+      key: const ValueKey(1),
+      startActionPane: const ActionPane(
+        motion: ScrollMotion(),
+        // dismissible: DismissiblePane(
+        //   onDismissed: () {},
+        // ),
         children: [
-          Text(
-            DateFormat('dd/MM/yyyy - HH:mm').format(task.taskDate),
-            style: const TextStyle(
-              fontSize: 12,
-            ),
-          ),
-          Text(
-            task.title,
-            style: const TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.w600,
-            ),
+          SlidableAction(
+            onPressed: null,
+            backgroundColor: Colors.red,
+            foregroundColor: Colors.white,
+            icon: Icons.delete,
+            label: 'Excluir',
           ),
         ],
+      ),
+      endActionPane: const ActionPane(
+        motion: ScrollMotion(),
+        children: [
+          SlidableAction(
+            flex: 2,
+            backgroundColor: Colors.red,
+            foregroundColor: Colors.white,
+            icon: Icons.delete,
+            label: 'Excluir',
+            onPressed: null,
+          ),
+        ],
+      ),
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          color: Colors.grey[200],
+        ),
+        margin: const EdgeInsets.symmetric(vertical: 3),
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              DateFormat('dd/MM/yyyy - HH:mm').format(task.taskDate),
+              style: const TextStyle(
+                fontSize: 12,
+              ),
+            ),
+            Text(
+              task.title,
+              style: const TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
